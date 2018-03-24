63331040166da9a06dd37459a047db282b1b88b9
himanshu dahiya <himanshudahiya@himanshus-MacBook-Pro.local>
2018-03-24 11:40:41 +0530

    commited

--- app/src/main/java/com/example/himanshudahiya/hackathon_cleaniness/TempActivity.java
+++ app/src/main/java/com/example/himanshudahiya/hackathon_cleaniness/TempActivity.java
@@ -1,39 +1,25 @@
 package com.example.himanshudahiya.hackathon_cleaniness;
 
-import android.content.Intent;
-import android.support.design.widget.BottomSheetDialogFragment;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
 
-import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
-import com.google.android.gms.common.GooglePlayServicesRepairableException;
-import com.google.android.gms.location.places.ui.PlacePicker;
+public class TempActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
 
-public class BottomSheet extends AppCompatActivity {
-
-    private Button bottomSheetButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_temp);
-
-        bottomSheetButton = (Button) findViewById(R.id.bottom_sheet_button);
-        bottomSheetButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                BottomSheetDialogFragment bottomSheetDialogFragment = new BottomSheetFragment();
-                bottomSheetDialogFragment.show(getSupportFragmentManager(), bottomSheetDialogFragment.getTag());
-            }
-        });
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -45,12 +31,9 @@ public class BottomSheet extends AppCompatActivity {
         toggle.syncState();
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener((NavigationView.OnNavigationItemSelectedListener) this);
+        navigationView.setNavigationItemSelectedListener(this);
     }
-    public void onClick(View view){
-        Intent intent=new Intent(this,ReviewsActivity.class);
-        startActivity(intent);
-    }
+
     @Override
     public void onBackPressed() {
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
